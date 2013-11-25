ed769b83b9474a6db15abc1e43aafd3fc7c7aad0
ThomasBoeriisSkole <twin159@gmail.com>
2013-11-25 14:16:23 +0100

    HTML Setup Dashboard

--- 4 Dages Prøve/NET/FormWorks/AdminPanel/App_Start/RouteConfig.cs
+++ 4 Dages Prøve/NET/FormWorks/AdminPanel/App_Start/RouteConfig.cs
@@ -16,8 +16,10 @@ namespace AdminPanel
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
